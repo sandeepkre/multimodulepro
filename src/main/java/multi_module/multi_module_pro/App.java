558e18ebc6eb2fd05aa8bde337d8c6b41d3305ce
Sandeep Reddy <sandeepkre@gmail.com>
2017-05-11 11:58:16 -0500

    --help

---
+++ src/main/java/multi_module/multi_module_pro/App.java
@@ -0,0 +1,13 @@
+package multi_module.multi_module_pro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
